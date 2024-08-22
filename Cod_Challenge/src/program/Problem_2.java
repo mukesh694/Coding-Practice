da5b2c55adc22e7c939711bf0f11f252f6022265
Mukesh <mukeshsin1997@gmail.com>
2024-08-22 13:28:47 +0530

    2nd problem

--- Cod_Challenge/src/program/Problem_2.java
+++ Cod_Challenge/src/program/Problem_2.java
@@ -3,9 +3,20 @@ package program;
 public class Problem_2 {
 
     public static void main (String[] args){
+//find the number of even number in a array
 
+        int [] a={2,8,3,7,15,89,90};
 
+        int count=0;
+        System.out.println(a.length);
 
+        for(int i=0;i<a.length;i++) {
+            if (a[i] % 2 == 0) {
+                count++;
+            }
+
+        }
+        System.out.println(count);
 
     }
 }
